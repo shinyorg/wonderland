f737e3d44ebc76bd48c5e1ecc4fc3155f9f9716a
Allan Ritchie <allan.ritchie@gmail.com>
2025-01-21 14:53:18 -0500

    Update to latest versions

--- ShinyWonderland/Extensions.cs
+++ ShinyWonderland/Extensions.cs
@@ -8,7 +8,7 @@ namespace ShinyWonderland
     {
         // https://api.themeparks.wiki/docs/v1.yaml
         // https://api.themeparks.wiki/v1/entity/66f5d97a-a530-40bf-a712-a6317c96b06d/live
-        public static Task<ExecutionResult<EntityLiveDataResponse>> GetWonderlandData(
+        public static Task<RequestResult<EntityLiveDataResponse>> GetWonderlandData(
             this IMediator mediator, 
             bool forceRefresh,
             CancellationToken cancellationToken

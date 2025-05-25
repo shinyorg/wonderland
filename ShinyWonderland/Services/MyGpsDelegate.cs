43cb2c0f5b64501770bdd5c0f2ffa3b05a35348b
Allan Ritchie <allan.ritchie@gmail.com>
2025-05-24 23:40:07 -0400

    GPS delegate will notify you it has been turned off

--- ShinyWonderland/Services/MyGpsDelegate.cs
+++ ShinyWonderland/Services/MyGpsDelegate.cs
@@ -1,25 +1,27 @@
+using Shiny.Notifications;
+
 namespace ShinyWonderland.Services;
 
 
 public class MyGpsDelegate : GpsDelegate
 {
+    readonly INotificationManager notifications;
     readonly IGpsManager gpsManager;
     readonly ParkOptions parkOptions;
     
     public MyGpsDelegate(
         ILogger<MyGpsDelegate> logger,
-        IConfiguration config,
         IOptions<ParkOptions> parkOptions,
+        INotificationManager notifications,
         IGpsManager gpsManager
     ) : base(logger)
     {
         this.MinimumDistance = Distance.FromMeters(10);
         this.MinimumTime = TimeSpan.FromSeconds(10);
 
-        var lat = config.GetValue<double>("Park:Latitude");
-        var lon = config.GetValue<double>("Park:Longitude");
         this.gpsManager = gpsManager;
         this.parkOptions = parkOptions.Value;
+        this.notifications = notifications;
     }
 
     
@@ -30,7 +32,12 @@ public class MyGpsDelegate : GpsDelegate
         {
             // shutter down
             this.Logger.LogInformation("Outside Wonderland, shutting down GPS");
+            
             await this.gpsManager.StopListener();
+            await this.notifications.Send(
+                "Wonderland Goodbye",
+                "GPS has been turned off to save battery - you will not receive real time ride updates"
+            );
         }
     }
 }
